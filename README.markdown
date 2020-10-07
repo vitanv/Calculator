2364f49257fd3bdd973e179105ec0642b2b19699
vitanv <vitanv@users.noreply.github.com>
2020-10-07 20:49:51 +0100

    initial commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# JavaScript Calculator
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/vitanv/pen/VwaxRbZ](https://codepen.io/vitanv/pen/VwaxRbZ).
+
+
---
+++ dist/script.js
@@ -0,0 +1,56 @@
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);_defineProperty(this, "clickNumber",
+
+
+
+
+
+    value => {
+      let val = 10 * this.state.display + Number(value);
+      console.log(val);
+      this.setState({
+        display: val });
+
+    });_defineProperty(this, "clickAC",
+    () => {
+      this.setState({
+        display: 0,
+        formula: 0 });
+
+    });this.state = { display: 0, formula: 0 };}
+  render() {
+    return (
+      React.createElement("div", { className: "App" },
+      React.createElement("div", { className: "container", id: "calculator" },
+      React.createElement("div", { className: "display", id: "formula" }, this.state.formula),
+      React.createElement("div", { className: "display", id: "display" }, this.state.display),
+      React.createElement("button", { className: "btn", id: "clear", onClick: this.clickAC }, "AC"),
+      React.createElement("button", { className: "btn", id: "delete" }, "CE"),
+      React.createElement("button", { className: "btn", id: "multiply" }, "x"),
+      React.createElement("button", { className: "btn", id: "seven", value: "7", onClick: e => this.clickNumber(e.target.value) }, "7"),
+      React.createElement("button", { className: "btn", id: "eight", value: "8", onClick: e => this.clickNumber(e.target.value) }, "8"),
+      React.createElement("button", { className: "btn", id: "nine", value: "9", onClick: e => this.clickNumber(e.target.value) }, "9"),
+      React.createElement("button", { className: "btn", id: "subtract", value: "-" }, "-"),
+      React.createElement("button", { className: "btn", id: "four", value: "4", onClick: e => this.clickNumber(e.target.value) }, "4"),
+      React.createElement("button", { className: "btn", id: "five", value: "5", onClick: e => this.clickNumber(e.target.value) }, "5"),
+      React.createElement("button", { className: "btn", id: "six", value: "6", onClick: e => this.clickNumber(e.target.value) }, "6"),
+      React.createElement("button", { className: "btn", id: "add", value: "+" }, "+"),
+      React.createElement("button", { className: "btn", id: "one", value: "1", onClick: e => this.clickNumber(e.target.value) }, "1"),
+      React.createElement("button", { className: "btn", id: "two", value: "2", onClick: e => this.clickNumber(e.target.value) }, "2"),
+      React.createElement("button", { className: "btn", id: "three", value: "3", onClick: e => this.clickNumber(e.target.value) }, "3"),
+      React.createElement("button", { className: "btn", id: "divide", value: "/" }, "/"),
+      React.createElement("button", { className: "btn", id: "zero", value: "0", onClick: e => this.clickNumber(e.target.value) }, "0"),
+      React.createElement("button", { className: "btn", id: "decimal", value: "." }, "."),
+      React.createElement("button", { className: "btn", id: "equals", value: "=" }, "="))));
+
+
+
+
+  }}
+
+
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
